7ef3b39154e9a68adfe92de984803f585f78a8cc
chuck.lei <chuck.lei@togic.com>
2015-01-06 10:33:13 +0800

    update install-zsh.sh

--- quick-install/install-zsh.sh
+++ quick-install/install-zsh.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 mkdir ~/opensource
+cd ~/opensource
 sudo apt-get install zsh autojump
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/opensource
-git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/opensource
-wget https://github.com/seaify/tools/blob/master/files/.zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
+cd ~
+wget https://raw.githubusercontent.com/seaify/tools/master/files/.zshrc
+source ~/.zshrc
+zsh
