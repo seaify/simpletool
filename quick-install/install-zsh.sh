9046f2b9f8e11fc1bab5ea415b13aaf5cb2e23a6
chuck.lei <chuck.lei@togic.com>
2015-01-06 11:09:09 +0800

    add git-promot

--- quick-install/install-zsh.sh
+++ quick-install/install-zsh.sh
@@ -5,6 +5,7 @@ sudo apt-get install zsh autojump
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
+git clone https://github.com/olivierverdier/zsh-git-prompt.git
 cd ~
 wget https://raw.githubusercontent.com/seaify/tools/master/files/.zshrc
 source ~/.zshrc
