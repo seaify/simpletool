9645a15ede269cd661131b27ed4e34aa8e551774
seaify <dilin.life@gmail.com>
2015-01-17 11:41:26 +0800

    Update install-zsh.sh

--- quick-install/install-zsh.sh
+++ quick-install/install-zsh.sh
@@ -8,6 +8,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
 git clone https://github.com/olivierverdier/zsh-git-prompt.git
 cd ~
-wget https://raw.githubusercontent.com/seaify/tools/master/files/.zshrc
-source ~/.zshrc
+wget https://raw.githubusercontent.com/seaify/tools/master/files/.zshrc -O ~/.zshrc
 zsh
