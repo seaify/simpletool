03865225c3970fbb435de7971b6890c68321622b
chuck.lei <chuck.lei@togic.com>
2015-01-17 23:42:27 +0800

    fix install scrapy bug

--- quick-install/centos/install-scrapy.sh
+++ quick-install/centos/install-scrapy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #for centos
 yum update -y
-yum install -y wget curl gcc build-essential libxml2-dev libxslt1-dev python-dev python-lxml libffi-devel python-devel libxml2-devel libxslt-devel
+yum install -y openssl-devel wget curl gcc build-essential libxml2-dev libxslt1-dev python-dev python-lxml libffi-devel python-devel libxml2-devel libxslt-devel
 curl https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py | python
+wget http://launchpadlibrarian.net/58498441/pyOpenSSL-0.11.tar.gz -P /opt
+cd /opt
+tar zxvf pyOpenSSL-0.11.tar.gz 
+cd pyOpenSSL-0.11
+python setup.py install
 pip install service_identity scrapy beautifulsoup4
