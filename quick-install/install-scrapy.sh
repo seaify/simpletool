cb4d2671dd7084cf0872e15d9a8fe6b65c1cc145
seaify <dilin.life@gmail.com>
2015-01-17 11:59:51 +0800

    Update install-scrapy.sh

--- quick-install/install-scrapy.sh
+++ quick-install/install-scrapy.sh
@@ -4,3 +4,4 @@ apt-get update
 apt-get install --yes gcc python-pip build-essential libxml2-dev libxslt1-dev python-dev python-lxml
 pip install service_identity
 pip install scrapy
+pip install beautifulsoup4
