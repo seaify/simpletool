85020f2975f1151e91e08f7deaf08fddf478fb0e
chuck.lei <chuck.lei@togic.com>
2015-01-18 08:52:09 +0800

    add zpanel centos7 install

---
+++ quick-install/centos/install-zpanel-centos7.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+yum update -y
+yum install -y expect wget
+wget https://raw.githubusercontent.com/zpanel/installers/master/install/beta/CentOS_7/beta-Centos-7-10.1.1.sh  -O /root/10_1_1.sh
+wget https://raw.githubusercontent.com/seaify/tools/master/quick-install/centos/second-install-zpanel.sh -O /root/second-install-zpanel.sh
+chmod +x /root/second-install-zpanel.sh
+./root/second-install-zpanel.sh
