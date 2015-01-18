8a889aa1767b3c8d7d0c924006449ab8fae23b74
chuck.lei <chuck.lei@togic.com>
2015-01-18 09:01:33 +0800

    fix path bug

--- quick-install/centos/install-zpanel-centos7.sh
+++ quick-install/centos/install-zpanel-centos7.sh
@@ -4,4 +4,5 @@ yum install -y expect wget
 wget https://raw.githubusercontent.com/zpanel/installers/master/install/beta/CentOS_7/beta-Centos-7-10.1.1.sh  -O /root/10_1_1.sh
 wget https://raw.githubusercontent.com/seaify/tools/master/quick-install/centos/second-install-zpanel.sh -O /root/second-install-zpanel.sh
 chmod +x /root/second-install-zpanel.sh
-./root/second-install-zpanel.sh
+cd /root
+./second-install-zpanel.sh
