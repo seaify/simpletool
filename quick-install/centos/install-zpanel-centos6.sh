102d4df04ab9228d87d910d3207ac4c8d1051245
chuck.lei <chuck.lei@togic.com>
2015-01-18 09:35:21 +0800

    add mysql install for centos

--- quick-install/centos/install-zpanel-centos6.sh
+++ quick-install/centos/install-zpanel-centos6.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 yum update -y
-yum install -y expect wget
+yum install -y expect wget mysql-community-server
+yum install -y http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+systemctl start mysqld
 wget https://raw.github.com/zpanel/installers/master/install/CentOS-6_4/10_1_1.sh -O /root/10_1_1.sh
 wget https://raw.githubusercontent.com/seaify/tools/master/quick-install/centos/second-install-zpanel.sh -O /root/second-install-zpanel.sh
 chmod +x /root/second-install-zpanel.sh
