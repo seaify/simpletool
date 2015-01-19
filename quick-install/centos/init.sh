ebfd14fe4bc8185abfd5d67897e82de5c51cc971
chuck.lei <chuck.lei@togic.com>
2015-01-19 20:17:40 +0800

    add composer to init script

--- quick-install/centos/init.sh
+++ quick-install/centos/init.sh
@@ -4,3 +4,5 @@ yum install -y git zsh autojump
 git config --global user.email "dilin.life@gmail.com"
 git config --global user.name "chuck.lei"
 wget https://raw.githubusercontent.com/seaify/tools/master/files/.vimrc -O ~/.vimrc
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
