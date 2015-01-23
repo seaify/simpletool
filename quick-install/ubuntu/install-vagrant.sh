27b8433a9e4ac346ec3c384a951f10a51654c943
chuck.lei <chuck.lei@togic.com>
2015-01-23 14:34:11 +0800

    fix virtualbox install problem

--- quick-install/ubuntu/install-vagrant.sh
+++ quick-install/ubuntu/install-vagrant.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
 sudo dpkg -i vagrant_1.7.2_x86_64.deb
+wget http://download.virtualbox.org/virtualbox/4.3.20/virtualbox-4.3_4.3.20-96996~Ubuntu~raring_amd64.deb
+sudo dpkg -i virtualbox-4.3_4.3.20-96996\~Ubuntu\~raring_amd64.deb
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list'
+sudo apt-get update
+sudo apt-get install virtualbox-4.3
