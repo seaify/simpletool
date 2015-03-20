2853b9d4585cd1035b1ef906b2c3f6a44f137254
chuck.lei <chuck.lei@togic.com>
2015-03-20 15:18:31 +0800

    update

--- quick-install/ubuntu/install-pptp.sh
+++ quick-install/ubuntu/install-pptp.sh
@@ -1,10 +1,10 @@
 !/bin/sh
 #for ubuntu14.04
-apt-get update
-apt-get install --yes pptpd
+apt-get install --yes pptpd pptp-linux
 wget https://raw.githubusercontent.com/seaify/tools/master/files/pptpd.conf -O /etc/pptpd.conf
 wget https://raw.githubusercontent.com/seaify/tools/master/files/pptpd-options -O /etc/ppp/pptpd-options
 wget https://raw.githubusercontent.com/seaify/tools/master/files/chap-secrets -O /etc/ppp/chap-secrets
 wget https://raw.githubusercontent.com/seaify/tools/master/files/sysctl.conf -O /etc/sysctl.conf
 wget https://raw.githubusercontent.com/seaify/tools/master/files/rc.local -O /etc/rc.local
+modprobe nf_conntrack_proto_gre nf_conntrack_pptp
 /etc/init.d/pptpd restart
