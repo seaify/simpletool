3c5bbe31daefe2fe9efd7093b1187d31d4a59c1f
chuck.lei <chuck.lei@togic.com>
2015-03-23 15:11:59 +0800

    quick install shadowsocks work

--- quick-install/ubuntu/install-shadowsock-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get --yes install python-pip python-m2crypto supervisor
-pip install shadowsocks
-$ip=`curl http://icanhazip.com/`
-//sed replace better
-echo $ip
--- quick-install/ubuntu/install-shadowsock-server.sh
+++ quick-install/ubuntu/install-shadowsock-server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-sudo apt-get update
 sudo apt-get --yes install python-pip python-m2crypto supervisor
 pip install shadowsocks
-$ip=`curl http://icanhazip.com/`
+ip=$(curl http://icanhazip.com/)
 echo $ip
+curl -X POST http://template.seaify.com/get_template --data "template_id=1&server=$ip&password=$1" > /etc/shadowsocks-config.json
+config="\n[program:shadowsocks]\ncommand=ssserver -c /etc/shadowsocks-config.json\nautorestart=true\nuser=root"
+echo $config > /etc/supervisor/conf.d/shadowsocks.conf
+service supervisor restart
