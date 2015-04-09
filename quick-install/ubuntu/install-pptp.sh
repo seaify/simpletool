5a5ac99a53b7fc2fb9f48385a0fcacc0204f2a78
chuck.lei <dilin.life@gmail.com>
2015-04-09 17:29:27 +0800

    update

--- quick-install/ubuntu/install-pptp.sh
+++ quick-install/ubuntu/install-pptp.sh
@@ -3,7 +3,7 @@
 apt-get install --yes pptpd pptp-linux
 wget https://raw.githubusercontent.com/seaify/tools/master/files/pptpd.conf -O /etc/pptpd.conf
 wget https://raw.githubusercontent.com/seaify/tools/master/files/pptpd-options -O /etc/ppp/pptpd-options
-cat "$1 * $2 *" > /etc/ppp/chap-secrets
+echo "$1 * $2 *" >> /etc/ppp/chap-secrets
 #wget https://raw.githubusercontent.com/seaify/tools/master/files/chap-secrets -O /etc/ppp/chap-secrets
 wget https://raw.githubusercontent.com/seaify/tools/master/files/sysctl.conf -O /etc/sysctl.conf
 wget https://raw.githubusercontent.com/seaify/tools/master/files/rc.local -O /etc/rc.local
