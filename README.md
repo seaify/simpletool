42fa9d6def1fa8d148d2f537d5c18d81ff6f251d
chuck.lei <dilin.life@gmail.com>
2015-04-09 17:24:26 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-tools
-=====
+开发过程中常用的工具，如一键安装vpn, ss, zsh, 以及配置文件如vimrc, zshrc等, 注意以下安装脚本仅在ubuntu 14.04下经过测试。
 
-common used tools for backend
+## 一键安装脚本
+
+### 安装pptp vpn
+根据文档https://help.ubuntu.com/community/PPTPServer来安装pptp
+```
+curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-pptp.sh | sh
+```
+
+
+### 安装shadowsocks server
+ curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-shadowsock-server.sh | sh
+
+### 安装vsftp
+ curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-ftp.sh | sh
+
+### 安装zsh, 已经配置好相应插件
+ curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-zsh.sh | sh
--- quick-install/ubuntu/install-pptp.sh
+++ quick-install/ubuntu/install-pptp.sh
@@ -3,7 +3,8 @@
 apt-get install --yes pptpd pptp-linux
 wget https://raw.githubusercontent.com/seaify/tools/master/files/pptpd.conf -O /etc/pptpd.conf
 wget https://raw.githubusercontent.com/seaify/tools/master/files/pptpd-options -O /etc/ppp/pptpd-options
-wget https://raw.githubusercontent.com/seaify/tools/master/files/chap-secrets -O /etc/ppp/chap-secrets
+cat "$1 * $2 *" > /etc/ppp/chap-secrets
+#wget https://raw.githubusercontent.com/seaify/tools/master/files/chap-secrets -O /etc/ppp/chap-secrets
 wget https://raw.githubusercontent.com/seaify/tools/master/files/sysctl.conf -O /etc/sysctl.conf
 wget https://raw.githubusercontent.com/seaify/tools/master/files/rc.local -O /etc/rc.local
 modprobe nf_conntrack_proto_gre nf_conntrack_pptp
