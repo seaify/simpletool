4a39c7e27a2e49c601d28f4fe73af6e3dbcae32a
chuck.lei <dilin.life@gmail.com>
2015-04-10 09:25:36 +0800

    fix curl save file permission bug

--- quick-install/ubuntu/install-ftp.sh
+++ quick-install/ubuntu/install-ftp.sh
@@ -2,7 +2,8 @@
 sudo apt-get update
 sudo apt-get --yes install wget vsftpd
 sudo useradd $1
-mkdir "/home/$1"
+sudo mkdir "/home/$1"
+sudo chown "$1:$1" "/home/$1"
 sudo echo "$1:$2" | /usr/sbin/chpasswd
 sudo rm /etc/pam.d/vsftpd
 sudo wget https://raw.githubusercontent.com/seaify/tools/master/files/vsftpd.conf -O /etc/vsftpd.conf
--- quick-install/ubuntu/install-quick-dev.sh
+++ quick-install/ubuntu/install-quick-dev.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-zsh.sh | sudo sh
-curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-pip-1.3.sh | sudo sh
+sudo apt-get install --yes unzip build-essential mysql-client-5.5 python-mysqldb libxml2-dev libxslt1-dev python-dev redis-server nginx gcc python-pip  python-lxml python-tk watchdog
+sudo useradd $1
+sudo mkdir "/home/$1"
+sudo chown "$1:$1" "/home/$1"
+sudo echo "$1:$2" | /usr/sbin/chpasswd
+curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-zsh.sh | sh
+curl https://raw.githubusercontent.com/seaify/tools/master/quick-install/ubuntu/install-pip-1.3.sh | sh
+sudo pip install werkzeug jinja2 celery service_identity scrapy beautifulsoup4 wechat_sdk supervisor gunicorn flask redis
 sudo mkdir -p /etc/supervisord/conf.d
 sudo wget https://raw.githubusercontent.com/seaify/tools/master/files/supervisord.conf -O /etc/supervisord.conf 
-sudo wget https://raw.githubusercontent.com/seaify/tools/master/files/.gitconfig -O ~/.gitconfig
-sudo wget https://raw.githubusercontent.com/seaify/tools/master/files/.vimrc -O ~/.vimrc
-sudo apt-get install --yes unzip build-essential mysql-client-5.5 python-mysqldb libxml2-dev libxslt1-dev python-dev redis-server nginx gcc python-pip  python-lxml python-tk watchdog
+wget https://raw.githubusercontent.com/seaify/tools/master/files/.gitconfig -O ~/.gitconfig
+wget https://raw.githubusercontent.com/seaify/tools/master/files/.vimrc -O ~/.vimrc
 #mysql-server-5.5
-sudo pip install werkzeug jinja2 celery service_identity scrapy beautifulsoup4 wechat_sdk supervisor gunicorn flask redis
+
