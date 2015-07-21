4291d2400332dbd8c66e630af7af550b9fc62322
Your Name <you@example.com>
2015-07-20 21:42:01 -0400

    add phpmyadmin

--- lib/simpletool.rb
+++ lib/simpletool.rb
@@ -10,7 +10,7 @@ class SimpleTool < Thor
 
   no_commands do
     def cp_file(src, dest, sudo=false)
-      command = 'cp ' + SimpleTool.source_root + '/' + src + ' ' + dest
+      command = 'cp -r ' + SimpleTool.source_root + '/' + src + ' ' + dest
       command = 'sudo ' + command if sudo
       ap command
       `#{command}`
@@ -19,7 +19,7 @@ class SimpleTool < Thor
 
 
 
-  desc "install_pptp_vpn", "quick install pptp vpn on ubuntu"
+  desc "install_pptp_vpn username passwd", "quick install pptp vpn on ubuntu, need provide username & password, remember to reboot machine to check!!!"
   def install_pptp_vpn
     `sudo apt-get install --yes pptpd pptp-linux`
     cp_file('support/pptp_vpn/ubuntu/pptpd.conf', '/etc/pptpd.conf', true)
@@ -29,10 +29,29 @@ class SimpleTool < Thor
     cp_file('support/pptp_vpn/ubuntu/rc.local', '/etc/rc.local', true)
     `sudo modprobe nf_conntrack_proto_gre nf_conntrack_pptp`
     `sudo /etc/init.d/pptpd restart`
+    `echo 'pleaese reboot to make it works'`
   end
 
   desc "install_phpmyadmin", "quick install phpmyadmin on ubuntu"
   def install_phpmyadmin
+    `sudo apt-get -y install nginx php5-cli php5-cgi php5-fpm php5-mcrypt php5-mysql php5-gd php-apc php5-common`
+
+    #modify mysql.ini
+    `sudo sed -i'' -e '/bind-address/d' /etc/mysql/my.cnf`
+    #
+    ##wget nginx default
+    #sudo wget https://raw.githubusercontent.com/seaify/tools/master/files/nginx.phpmyadmin.default -O /etc/nginx/sites-enabled/default
+    cp_file('support/phpmyadmin/nginx.phpmyadmin.default',  '/etc/nginx/sites-enabled/nginx.phpmyadmin.default', true)
+    cp_file('support/phpmyadmin/phpmyadmin',  '/usr/share/nginx/html/', true)
+    #
+    ##download phpmyadmin files
+    #cd /usr/share/nginx/html
+    #sudo wget http://superb-dca2.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.4.8/phpMyAdmin-4.4.8-all-languages.tar.bz2
+    #sudo tar xvf phpMyAdmin-4.4.8-all-languages.tar.bz2
+    #sudo mv phpMyAdmin-4.4.8-all-languages phpmyadmin
+    #
+    `sudo service nginx reload`
+    `sudo service php5-fpm restart`
 
   end
 
